745490c950e6adaaea7dbf034e777f0c5b564915
Daniel Tibbotts <dan.tibbotts@gmail.com>
2022-08-25 15:06:46 +1200

    fixed styling issues

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,22 +5,23 @@ const Hero = () => {
   return (
     <section id="hero">
       <div className="hero-grid">
-        <div>
+
+        <div className="hero-panel">
           <span id="hero-title">Software Developer</span>
           <span id="hero-subtitle">Web, Desktop, Mobile</span>
           <span id="hero-text">
             Providing real world solutions for real life problems
           </span>
-
           <div className="hero-cta">
             <a href="#projects" className="btn btn-primary">Projects</a> 
             <a href="#contact" className="btn btn-secondary">Contact Me</a> 
           </div>
-          
         </div>
+
         <div className="hero-image-container">
           <img id="hero-image" src={HeroImage} />
         </div>
+
       </div>
     </section>
   );
