6bab4b4149d4b6638d43a0b1a17048539adfab3b
Daniel Tibbotts <dan.tibbotts@gmail.com>
2022-08-26 07:14:36 +1200

    styled navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,31 +1,46 @@
-import { useState } from 'react';
-import {React} from 'react'
+import { useState } from "react";
+import { React } from "react";
 import "./navbar.css";
 
 const Navbar = () => {
   const [color, setColor] = useState();
 
   const changeColor = () => {
-    if (window.scrollY >= (window.innerHeight - 100)){
-      setColor(true)
+    if (window.scrollY >= window.innerHeight - 100) {
+      setColor(true);
     } else {
-      setColor(false)
+      setColor(false);
     }
-  }
+  };
 
-  window.addEventListener('scroll', changeColor);
+  window.addEventListener("scroll", changeColor);
 
   return (
-    <nav className={color ? 'nav-bg' : ''}>
-      <div></div>
-      <div className='nav-menu'>
-        <a href="#">Home</a>
-        <a href="#about">About</a>
-        <a href="#projects">Projects</a>
-        <a href="#contact">Contact Me</a>
+    <nav className={color ? "nav-bg" : ""}>
+      <div className="nav-menu">
+        
+        <a href="#" className="nav-menu-item">
+          <i class="fa-solid fa-house"></i>
+          <span>Home</span>
+        </a>
+
+        <a className="nav-menu-item" href="#about">
+          <i class="fa-solid fa-user"></i>
+          <span>About</span>
+        </a>
+
+        <a href="#projects" className="nav-menu-item">
+          <i class="fa-solid fa-tablet-button"></i>
+          <span>Projects</span>
+        </a>
+
+        <a href="#contact" className="nav-menu-item">
+          <i class="fa-solid fa-address-card"></i>
+          <span>Contact</span>
+        </a>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
